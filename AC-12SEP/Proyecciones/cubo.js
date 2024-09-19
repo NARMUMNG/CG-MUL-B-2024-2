7393ab00316d020e57e1a25b7db1d4c32de3f793
NARMUMNG <157733705+NARMUMNG@users.noreply.github.com>
2024-09-19 13:11:46 -0500

    Creacion del js

---
+++ AC-12SEP/Proyecciones/cubo.js
@@ -0,0 +1,18 @@
+// Clase Linea
+class Linea {
+    constructor(x1, y1, x2, y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    dibujar(ctx, color = 'black') {
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(this.x1, this.y1);
+        ctx.lineTo(this.x2, this.y2);
+        ctx.stroke();
+    }
+}
+
